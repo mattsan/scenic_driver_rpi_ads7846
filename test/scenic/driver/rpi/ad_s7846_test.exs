2c4fda21c52a34201ccd07c93db8eb390214b2cb
Eiji MATSUMOTO <e.mattsan@gmail.com>
2020-10-17 18:13:01 +0900

    initial commit for a driver

--- test/scenic/driver/rpi/ad_s7846_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Scenic.Driver.Rpi.ADS7846Test do
-  use ExUnit.Case
-  doctest Scenic.Driver.Rpi.ADS7846
-
-  test "greets the world" do
-    assert Scenic.Driver.Rpi.ADS7846.hello() == :world
-  end
-end
