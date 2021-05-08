fcec7785b9b30a6782867aa1c0f73b58b3e44cc6
Eiji MATSUMOTO <e.mattsan@gmail.com>
2021-05-08 15:44:26 +0900

    extract environment specific codes

--- lib/scenic/driver/rpi/ads7846.ex
+++ lib/scenic/driver/rpi/ads7846.ex
@@ -6,6 +6,7 @@ defmodule Scenic.Driver.Rpi.ADS7846 do
 
   require Logger
 
+  @init_retry_ms 400
   @device "ADS7846 Touchscreen"
   @initial_state %{
     device: @device,
@@ -43,11 +44,19 @@ defmodule Scenic.Driver.Rpi.ADS7846 do
   def handle_info(_, _)
 
   def handle_info({:init_driver, requested_device}, state) do
-    state =
-      state
-      |> Device.initialize(requested_device)
+    case Device.initialize(requested_device) do
+      {:ok, {event_pid, event_path}} ->
+        Logger.info("ADS7846 Driver initialized")
 
-    {:noreply, state}
+        {:noreply, %{state | event_pid: event_pid, event_path: event_path}}
+
+      :not_found ->
+        Logger.warning("#{requested_device} not found. Retry to find it.")
+
+        Process.send_after(self(), {:init_driver, requested_device}, @init_retry_ms)
+
+        {:noreply, state}
+    end
   end
 
   def handle_info({:input_event, event_path, events}, %{event_path: event_path} = state) do
--- lib/scenic/driver/rpi/ads7846/device.ex
+++ lib/scenic/driver/rpi/ads7846/device.ex
@@ -3,16 +3,9 @@ defmodule Scenic.Driver.Rpi.ADS7846.Device do
 
   require Logger
 
-  def initialize(state, requested_device) do
-    device_info = find_device(requested_device)
-    init_driver(state, device_info)
-  end
-
   case Mix.target() do
     target when target in [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4] ->
-      @init_retry_ms 400
-
-      defp find_device(requested_device) do
+      def initialize(requested_device) do
         device_info =
           InputEvent.enumerate()
           |> Enum.find(fn {_, %InputEvent.Info{name: name}} ->
@@ -20,39 +13,18 @@ defmodule Scenic.Driver.Rpi.ADS7846.Device do
           end)
 
         case device_info do
-          {_event, %InputEvent.Info{}} ->
-            device_info
-
-          _ ->
-            {:not_found, requested_device}
-        end
-      end
-
-      defp init_driver(state, device_info) do
-        case device_info do
           {event_path, %InputEvent.Info{}} ->
             {:ok, event_pid} = InputEvent.start_link(event_path)
+            {:ok, {event_pid, event_path}}
 
-            Logger.info("ADS7846 Driver initialized")
-
-            %{state | event_pid: event_pid, event_path: event_path}
-
-          {:not_found, requested_device} ->
-            Logger.warning("#{requested_device} not found. Retry to find it.")
-
-            Process.send_after(self(), {:init_driver, requested_device}, @init_retry_ms)
-
-            state
+          _ ->
+            :not_found
         end
       end
 
     _ ->
-      defp find_device(_) do
-        :ok
-      end
-
-      defp init_driver(state, :ok) do
-        state
+      def initialize(_) do
+        {:ok, {:event_pid, :event_path}}
       end
   end
 end
